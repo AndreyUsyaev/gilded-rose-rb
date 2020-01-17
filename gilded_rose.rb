88129369db47199b1dc98eb0d0543a0ce72f8615
UsyaevAndrey <andrey.usyaev@gmail.com>
2020-01-17 22:46:13 +0300

    Refactor Gilded Rose kata

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -3,52 +3,8 @@ class GildedRose
     @items = items
   end
 
-  def update_quality()
-    @items.each do |item|
-      if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
-        if item.quality > 0
-          if item.name != 'Sulfuras, Hand of Ragnaros'
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != 'Sulfuras, Hand of Ragnaros'
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            if item.quality > 0
-              if item.name != 'Sulfuras, Hand of Ragnaros'
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+  def update_quality
+    @items.each { |item| ItemUpdateService.new(item).call }
   end
 end
 
@@ -63,5 +19,55 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class ItemUpdateService
+  MIN_QUALITY = 0
+  MAX_QUALITY = 50
+  DEFAULT_QUALITY_ADJUSTMENT = 1
+
+  def initialize(item)
+    @item = item
+  end
+
+  def call
+    return if @item.name == 'Sulfuras, Hand of Ragnaros'
+
+    decrease_sell_in
+    update_quality
+    validate_quality
+  end
+
+  def update_quality
+    adjustment = DEFAULT_QUALITY_ADJUSTMENT
+    adjustment *= 2 if @item.sell_in < 0
+
+    case @item.name
+    when 'Aged Brie'
+      @item.quality += adjustment
+    when 'Backstage passes to a TAFKAL80ETC concert'
+      if @item.sell_in >= 0
+        adjustment += 1 if @item.sell_in < 10
+        adjustment += 1 if @item.sell_in < 5
+        @item.quality += adjustment
+      else
+        @item.quality = 0
+      end
+    when 'Conjured Mana Cake'
+      adjustment *= 2
+      @item.quality -= adjustment
+    else
+      @item.quality -= adjustment
+    end
+  end
+
+  def decrease_sell_in
+    @item.sell_in -= 1
+  end
+
+  def validate_quality
+    @item.quality = MIN_QUALITY if @item.quality < MIN_QUALITY
+    @item.quality = MAX_QUALITY if @item.quality > MAX_QUALITY
   end
 end
